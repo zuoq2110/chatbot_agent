63e4bcb05030a635a868ae4252c90c6a35ca98b2
ductrong <trongdeck@gmail.com>
2025-09-11 19:21:08 +0700

    add email1

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -1,17 +1,16 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field,EmailStr
 from typing import Optional
 from datetime import datetime
 
 
 class UserCreate(BaseModel):
     username: str
-    email:str
     password: str  # Raw password (sẽ được hash)
     student_code: Optional[str] = None
     student_name: Optional[str] = None
     student_class: Optional[str] = None
     role: Optional[str] = "user"  # Thêm role, mặc định là "user"
-    email: Optional[str] = None
+    email: EmailStr = Field(..., description="Email là bắt buộc")
 
 
 class UserLogin(BaseModel):
